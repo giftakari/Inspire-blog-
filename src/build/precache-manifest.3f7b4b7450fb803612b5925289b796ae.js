188c50f9b09707156192899650528fc0023e7b66
Godsgift ThankGod Akari <giftakari@yahoo.com>
2019-09-23 15:09:52 +1000

    First Commit

---
+++ src/build/precache-manifest.3f7b4b7450fb803612b5925289b796ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc4d10b03e4f7f982da2f54db71df66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb1fb3fa61b18846273b",
+    "url": "/static/css/main.01f8efe7.chunk.css"
+  },
+  {
+    "revision": "64a2bc226100a338cc86",
+    "url": "/static/js/2.017a63f2.chunk.js"
+  },
+  {
+    "revision": "bb1fb3fa61b18846273b",
+    "url": "/static/js/main.7e99e1ed.chunk.js"
+  },
+  {
+    "revision": "b62bf97883e0354ac053",
+    "url": "/static/js/runtime-main.71ecb059.js"
+  }
+]);
